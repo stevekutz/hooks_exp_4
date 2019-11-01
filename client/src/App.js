5f192d31b431123f0a5983e5d9af45cc82fcd9e9
stevekutz <stkutz@gmail.com>
2019-10-31 22:47:09 -0500

    built horz drag and drop using hooks

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,14 @@ import React from 'react';
 import HooksTwoList from './HooksTwoList';
 import StateFullTwoList from './StateFullTwoList';
 import StateFullHorzList from './StateFullHorzList';
+import HooksHorz from './HooksHorz';
 
 function App() {
 
   return (
     <div>
-      
-        <StateFullHorzList/>
+        <HooksHorz/>
+       
     </div>
   );
 }
--- client/src/HooksHorz.js
+++ client/src/HooksHorz.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 // fake data generator
-const getItems = count =>
-  Array.from({ length: count }, (v, k) => k).map(k => ({
+const getItems = count => Array.from({ length: count }, (v, k) => k).map(k => ({
     id: `item-${k}`,
     content: `item ${k}`,
   }));
@@ -39,40 +38,84 @@ const getListStyle = isDraggingOver => ({
   overflow: 'auto',
 });
 
+
+
 function HooksHorz () {
+    const[items, setItems] = useState(getItems(6))
     
-
-
+    
     //   constructor(props) {
-//     super(props);
-//     this.state = {
-//       items: getItems(6),
-//     };
-//     this.onDragEnd = this.onDragEnd.bind(this);
-//   }
+        //     super(props);
+        //     this.state = {
+            //       items: getItems(6),
+            //     };
+            //     this.onDragEnd = this.onDragEnd.bind(this);
+            //   }
 
-  onDragEnd(result) {
-    // dropped outside the list
-    if (!result.destination) {
-      return;
-    }
 
-    const items = reorder(
-      this.state.items,
-      result.source.index,
-      result.destination.index
-    );
 
-    this.setState({
-      items,
-    });
+            useEffect(() => {
+                console.log("items is ", items);
+            }, items); 
+            
+            const onDragEnd = (result) => {
+                // dropped outside the list
+                if (!result.destination) {
+                    return;
+                }
+                
+                const itemsNew = reorder(
+                    items,
+                    result.source.index,
+                    result.destination.index
+                    );
+                    
+                    setItems(itemsNew)
+                }
+                
+                // Normally you would want to split things out into separate components.
+                // But in this example everything is just done in one place for simplicity
+                
+        return (
+            <DragDropContext onDragEnd={onDragEnd}>
+            <Droppable droppableId="droppable" direction="horizontal">
+              {(provided, snapshot) => (
+                <div
+                  ref={provided.innerRef}
+                  style={getListStyle(snapshot.isDraggingOver)}
+                  {...provided.droppableProps}
+                >
+                  {items.map((item, index) => (
+                    <Draggable key={item.id} draggableId={item.id} index={index}>
+                      {(provided, snapshot) => (
+                        <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          style={getItemStyle(
+                            snapshot.isDragging,
+                            provided.draggableProps.style
+                          )}
+                        >
+                          {item.content}
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+          </DragDropContext>
+        )
+ 
   }
 
-  // Normally you would want to split things out into separate components.
-  // But in this example everything is just done in one place for simplicity
-  render() {
-    return (
-      <DragDropContext onDragEnd={this.onDragEnd}>
+
+export default HooksHorz;
+
+/*
+      <DragDropContext onDragEnd={onDragEnd}>
         <Droppable droppableId="droppable" direction="horizontal">
           {(provided, snapshot) => (
             <div
@@ -80,7 +123,7 @@ function HooksHorz () {
               style={getListStyle(snapshot.isDraggingOver)}
               {...provided.droppableProps}
             >
-              {this.state.items.map((item, index) => (
+              {items.map((item, index) => (
                 <Draggable key={item.id} draggableId={item.id} index={index}>
                   {(provided, snapshot) => (
                     <div
@@ -102,8 +145,5 @@ function HooksHorz () {
           )}
         </Droppable>
       </DragDropContext>
-    );
-  }
-}
-
-export default HooksHorz;
+    )
+*/
